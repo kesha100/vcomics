709c1635d8b74e0489641062e900f6b2479bcf1e
Aruuke <arukeshxo@gmail.com>
2024-09-23 09:48:52 +0600

    cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,22 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-// import { UploadthingService } from './uploadthing/uploadthing.service';
 import * as requestIp from 'request-ip';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(requestIp.mw());
   const configService = app.get(ConfigService);
-  // const uploadthingService = app.get(UploadthingService);
 
-  // app.use("/api/uploadthing", uploadthingService.getRouteHandler());
   app.enableCors({
-    origin: '*', // Replace with your Vercel app's domain
+    origin: 'https://vcomics.vercel.app', // Your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
+
   const port = configService.get('PORT') || 3000;
   await app.listen(port);
 }
+
 bootstrap();
